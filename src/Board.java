a3086a10c758678698e85a5f7339bc78043bc715
Sami Mnif <66852892+Samimnif@users.noreply.github.com>
2022-10-16 23:31:07 -0400

    Editing the Box and Board class. Adding comments for clarity. Adding user input and a while loop - Sami

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,28 @@
-import java.lang.reflect.Array;
+/**
+ * Board class
+ * Board contains boxes objects.
+ * MAIN class
+ *
+ * @author Sami Mnif
+ * @version 2022-10-16
+ */
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Board {
     private ArrayList<ArrayList<Box>> board;
     private final int boardSize = 10;
 
+    /**
+     * Constructor of the Board object
+     * Creates a 10x10 board
+     * fill the board spots with Box objects.
+     *
+     */
     public Board() {
         this.board = new ArrayList<>(boardSize);
-        Box newBox;// = new Box('-');
+        Box newBox;
         for(int r = 0; r < boardSize; r++)  {
             board.add(new ArrayList<Box>(boardSize));
             for(int c = 0; c < boardSize; c++){
@@ -15,6 +30,11 @@ public class Board {
             }
         }
     }
+
+    /**
+     * prints the board contents to the terminal
+     *
+     */
     public void printBoard() {
         for(int r = 0; r < boardSize; r++) {
             for (int c = 0; c < boardSize; c++) {
@@ -26,8 +46,13 @@ public class Board {
     public static void main(String[] args) {
 
         Board table = new Board();
+        Scanner input = new Scanner(System.in);
+        String userInput = "";
         System.out.println("Welcome to Scrabble");
-        table.printBoard();
+        while(!(userInput.equals("exit"))) {
+            table.printBoard();
+            userInput = input.nextLine();
+        }
 
     }
 }
