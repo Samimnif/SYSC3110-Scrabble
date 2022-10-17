5ea36cb211e36683ea242ed487c583d57173ddd5
Sami Mnif <66852892+Samimnif@users.noreply.github.com>
2022-10-16 23:09:45 -0400

    Adding the board and box class. Initializing the board with just '-' character to display empty board. - Sami

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,33 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Board {
+    private ArrayList<ArrayList<Box>> board;
+    private final int boardSize = 10;
+
+    public Board() {
+        this.board = new ArrayList<>(boardSize);
+        Box newBox;// = new Box('-');
+        for(int r = 0; r < boardSize; r++)  {
+            board.add(new ArrayList<Box>(boardSize));
+            for(int c = 0; c < boardSize; c++){
+                board.get(r).add(newBox = new Box('-'));
+            }
+        }
+    }
+    public void printBoard() {
+        for(int r = 0; r < boardSize; r++) {
+            for (int c = 0; c < boardSize; c++) {
+                System.out.printf(" %c ",this.board.get(r).get(c).getLetter());
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+
+        Board table = new Board();
+        System.out.println("Welcome to Scrabble");
+        table.printBoard();
+
+    }
 }
---
+++ src/Box.java
@@ -0,0 +1,11 @@
+public class Box {
+    private char letter;
+
+    public Box(char letter) {
+        this.letter = letter;
+    }
+
+    public char getLetter() {
+        return this.letter;
+    }
+}
